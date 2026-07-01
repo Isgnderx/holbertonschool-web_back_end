0f5a06dd09306f7644cde8bea05d938d3702328e
Isgnderx <ayxanisgenderzade999@gmail.com>
2026-07-01 13:17:06 +0400

    salam

--- ES6_classes/0-classroom.js
+++ ES6_classes/0-classroom.js
@@ -1,9 +1,5 @@
-import ClassRoom from "./0-classroom.js";
-
-class ClassRoom {
+export default class ClassRoom {
   constructor(maxStudentsSize) {
     this._maxStudentsSize = maxStudentsSize;
+  }
 }
-
-const room = new ClassRoom(10);
-console.log(room._maxStudentsSize)
